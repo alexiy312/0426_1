62ef4d91ab38a4a4a76f1a9f923b5508f7615859
satoshigp <satoshi3104>
2019-04-26 16:04:00 +0900

    Initial commit

---
+++ 0426_1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _0426_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text="1日1回勝負";
+            MessageBox.Show("YOU LOSE\nなんで負けたか明日までに考えといてください\nそしたら何かが見えてくるはずです\nほな、頂きます");
+            button1.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.BackColor = Color.Aqua;
+            button2.Text = "(;O;)";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Location = new Point(1, 1);
+            button3.Size = new Size(2, 2);
+            MessageBox.Show("どこでしょうか？\nウィンドウ上にちゃんといます！");
+        }
+    }
+}
